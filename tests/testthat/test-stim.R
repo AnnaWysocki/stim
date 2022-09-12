ca6b791cf67a9e2b5c77528ae5d522f9bf81d07d
Anna Wysocki <awysocki@ucdavis.edu>
2022-09-12 13:07:37 -0700

    Add tests for stim() function when estimating a residual covariance

--- tests/testthat/test-stim.R
+++ tests/testthat/test-stim.R
@@ -1,37 +1,50 @@
-# Create a sample covariance matrix with known properties
+# Function for creating a sample covariance matrix with known properties
 
-GetSampleCov <- function(BPop, cnames){# Beta Matrix needs to be in regression format
+GetSampleCov <- function(B, ResidualCov){# Beta Matrix needs to be in regression format
   # rows are causes and columns are effects
 
-  Sigma1 <- diag(ncol(BPop))
+  Sigma1 <- diag(ncol(B))
   mat_res <- list(NULL)
 
   for (j in 1:1000){
-    psi <- diag(1 - diag(t(BPop) %*% Sigma1 %*% BPop), ncol(BPop)) # Creates a psi matrix
+    psi <- diag(1 - diag(t(B) %*% Sigma1 %*% B), ncol(B)) # Creates a psi matrix
     # so the variance = 1
-    if( sum(psi < 0) == 0){ # Only does the following steps if the psi values are
+
+    colnames(psi) <- rownames(psi) <- colnames(B)
+
+
+    for(i in 1:nrow(ResidualCov)){
+      psi[ResidualCov[i, 1],ResidualCov[i, 2]] <- ResidualCov[i, 3]
+      psi[ResidualCov[i, 2],ResidualCov[i, 1]] <- ResidualCov[i, 3]
+    }
+
+    if(sum(diag(psi) < 0) == 0){ # Only does the following steps if the psi values are
       # positive
-      Sigma2 <- t(BPop) %*% Sigma1 %*% BPop + psi
+      Sigma2 <- t(B) %*% Sigma1 %*% B + psi
       Sigma1 <- Sigma2
       mat_res[[j]] <- Sigma2
       #  #print(Sigma2)
-    }
-    else{}
+    }else{}
   }
   if(length(mat_res) == 1000){ # Checks if the previous for loop returned 1000
     # matrices which is expected if we have a stable
     # process
-    rownames(Sigma2) <- colnames(Sigma2) <- cnames
-    return(Sigma2)}
-  else{print("Non-stationary")}
+    rownames(Sigma2) <- colnames(Sigma2) <- colnames(B)
+
+    return(Sigma2)} else{print("Non-stationary")}
 }
+
+
+#######################################
+#  Tests when no residual covariance  #
+#######################################
 
 # 2 variable set up
 BPop2 <- matrix(c(.3,.3,
                   0,.3), ncol = 2, nrow = 2, byrow = TRUE)
 colnames(BPop2 ) <- c("X", "Y")
 rownames(BPop2 ) <- c("X_0", "Y_0")
-sampleCov2 <- GetSampleCov(BPop2, c("X", "Y"))
+sampleCov2 <- GetSampleCov(BPop2, data.frame("X", "Y", 0))
 
 model2 <-  'Y ~ X'
 stability <- data.frame(X = .3, Y = 0.33)
@@ -43,7 +56,7 @@ BPop3 <- matrix(c(.3,.3, .3,
                   0, 0, .3), ncol = 3, nrow = 3, byrow = TRUE)
 colnames(BPop3) <- c("X", "Y", "Z")
 rownames(BPop3) <- c("X_0", "Y_0", "Z_0")
-sampleCov3 <- GetSampleCov(BPop3, c("X", "Y", "Z"))
+sampleCov3 <- GetSampleCov(BPop3, data.frame("X", "Y", 0))
 
 
 model3 <-  'Y ~ X
@@ -74,13 +87,6 @@ test_that("stim function returns correct solution with 2 variables", {
 })
 
 
-test_that("stim function returns correct solution with 2 variables", {
-
-  ModelFit2 <- stim(S = sampleCov2, n = 1000, model = model2, stability = stability)
-  lavaanSolution2 <- t(round(lavaan::inspect(ModelFit2$lavaanObjects[[1]], what = "std")$lambda, 1))
-  expect_equal(unclass(lavaanSolution2), BPop2)
-})
-
 
 test_that("stim function returns correct solution with 3 variables", {
 
@@ -96,3 +102,65 @@ test_that("If the number of estimated parameters exceeds degrees of freedom, fun
   expect_error(stim(S = sampleCov2, n = 1000, effects = tooManyEffects, stability = stability2))
 
 })
+
+
+
+#######################################
+#   Tests when residual covariance    #
+#######################################
+
+# 2 variable set up
+BPop2 <- matrix(c(.3,.3,
+                  0,.3), ncol = 2, nrow = 2, byrow = TRUE)
+colnames(BPop2 ) <- c("X", "Y")
+rownames(BPop2 ) <- c("X_0", "Y_0")
+sampleCov2 <- GetSampleCov(BPop2, data.frame("X", "Y", .1))
+
+model2 <-  'Y ~ .3 * X
+            X ~~  Y'
+stability <- data.frame(X = .3, Y = 0.363)
+
+
+# 3 variable set up
+BPop3 <- matrix(c(.3,.3, .3,
+                  0,.3, .3,
+                  0, 0, .3), ncol = 3, nrow = 3, byrow = TRUE)
+colnames(BPop3) <- c("X", "Y", "Z")
+rownames(BPop3) <- c("X_0", "Y_0", "Z_0")
+sampleCov3 <- GetSampleCov(BPop3, data.frame("X", "Y", .05))
+
+
+model3 <-  'Y ~ .3 *X
+            Z ~ X + Y
+            X ~~ Y'
+stability3 <- data.frame(X = .3, Y = 0.346, Z = .406)
+
+
+
+# data setup
+dat <- data.frame(Y = rnorm(500, 0, 1), X = rnorm(500, 0, 1), Z = rnorm(500, 0, 1))
+
+
+# tests
+
+test_that("stim function returns correct solution with 2 variables & covarying residual", {
+
+  ModelFit2 <- stim(S = sampleCov2, n = 1000, model = model2, stability = stability)
+  lavaanSolution2 <- t(round(lavaan::inspect(ModelFit2$lavaanObjects[[1]], what = "std")$lambda, 1))
+  expect_equal(unclass(lavaanSolution2), BPop2)
+})
+
+
+test_that("stim function returns correct solution with 3 variables", {
+
+  ModelFit3 <- stim(S = sampleCov3, n = 1000, model = model3, stability = stability3)
+  lavaanSolution3 <- t(round(lavaan::inspect(ModelFit3$lavaanObjects[[1]], what = "std")$lambda, 1))
+  expect_equal(unclass(lavaanSolution3), BPop3)
+
+})
+
+
+
+
+
+
